b4a69b872c7cb6632fa55b0efbbaf3304f938ef5
Ital023 <contadoitalo911@gmail.com>
2024-10-11 21:46:42 -0300

    feat: ResourceNotFoundException

---
+++ src/main/java/com/devsuperior/demo/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.demo.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
