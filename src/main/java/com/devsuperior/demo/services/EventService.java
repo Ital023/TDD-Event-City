a394d9414aa8cd52ecad118dbaaa8a7b774c92b5
Ital023 <contadoitalo911@gmail.com>
2024-10-11 22:05:56 -0300

    feat: Event service update

---
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.EventRepository;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getReferenceById(id);
+            dtoToEntity(entity, dto);
+            entity = repository.save(entity);
+
+            return new EventDTO(entity);
+        }catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+    }
+
+    private void dtoToEntity(Event entity, EventDTO dto) {
+        entity.setName(dto.getName());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(new City(dto.getCityId(), null));
+        entity.setDate(dto.getDate());
+    }
+
+
+}
