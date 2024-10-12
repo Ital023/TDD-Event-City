0bf5b005c3301ac04406a1a160f8506a747d7a9a
Ital023 <contadoitalo911@gmail.com>
2024-10-11 22:06:07 -0300

    feat: Event controller update

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+}
